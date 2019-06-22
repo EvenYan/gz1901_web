a427943b4bf73b58268e72e8d48f6a73b1771899
even <EvenYan>
2019-06-22 13:54:46 +0800

    类管理器

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class PostManager(models.Manager):
+    def title_content(self, kv):
+        return self.filter(title__contains=kv)
+    def get_queryset(self):
+        return super().get_queryset().filter(is_delete=0)
 
 
 class Post(models.Model):
     title = models.CharField(max_length=50, verbose_name="标题")
     body = models.TextField(verbose_name="内容")
+    objects = PostManager()
+    is_delete = models.BooleanField(default=0)
 
     def __str__(self):
         return self.title
